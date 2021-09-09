de6e4aba211f3d99c67e6c00044bc489c699ee50
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-09-09 16:48:31 +0530

    AddressBook_ADO

---
+++ AddressBook_ADO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddressBookADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To AddressBook Data Base Program!");
+            AddressBookSQLRepo repo = new AddressBookSQLRepo();
+            AddressBookModel enter = new AddressBookModel();
+            repo.GetDetails();
+
+        }
+    }
+}
